9ab9e3e71dc0fcb785d7ed262c4486014860017f
fisecode <akhmatfikriseptiawan@gmail.com>
2023-03-08 00:28:28 +0700

    Update

--- public/app.js
+++ public/app.js
@@ -1,10 +1,21 @@
 const container = document.querySelector("#root");
 const root = ReactDOM.createRoot(container);
-function Tick() {
-  const time = new Date();
-  const timeNow = time.toLocaleTimeString();
-  return /*#__PURE__*/React.createElement("div", {
-    className: "box"
-  });
+function App() {
+  const [click, setClick] = React.useState(true);
+  const [count, setCount] = React.useState(0);
+  React.useEffect(function () {
+    console.log(document.getElementById("judul"));
+  }, [click]);
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", {
+    id: "judul"
+  }, "Hello ini Judul"), /*#__PURE__*/React.createElement("button", {
+    onClick: function () {
+      setClick(false);
+    }
+  }, "Click Here"), /*#__PURE__*/React.createElement("button", {
+    onClick: function () {
+      setCount(count + 1);
+    }
+  }, "Click Here"), "Nilai saat ini: ", count);
 }
-root.render( /*#__PURE__*/React.createElement(Tick, null));
+root.render( /*#__PURE__*/React.createElement(App, null));
--- src/app.js
+++ src/app.js
@@ -1,10 +1,37 @@
 const container = document.querySelector("#root");
 const root = ReactDOM.createRoot(container);
 
-function Tick() {
-  const time = new Date();
-  const timeNow = time.toLocaleTimeString();
-  return <div className="box"></div>;
+function App() {
+  const [click, setClick] = React.useState(true);
+  const [count, setCount] = React.useState(0);
+
+  React.useEffect(
+    function () {
+      console.log(document.getElementById("judul"));
+    },
+    [click]
+  );
+
+  return (
+    <>
+      <h1 id="judul">Hello ini Judul</h1>
+      <button
+        onClick={function () {
+          setClick(false);
+        }}
+      >
+        Click Here
+      </button>
+      <button
+        onClick={function () {
+          setCount(count + 1);
+        }}
+      >
+        Click Here
+      </button>
+      Nilai saat ini: {count}
+    </>
+  );
 }
 
-root.render(<Tick />);
+root.render(<App />);
